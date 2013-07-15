21194639daf8336b0f1c0f75bfae53dcc5cc8e90
Dub <Dub@192.168.2.2>
2013-07-15 10:41:54 -0700

    initial commit

---
+++ src/main/java/org/jtp/vlcfxmediaplayer/VLCFXMediaPlayer.java
@@ -0,0 +1,29 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.jtp.vlcfxmediaplayer;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Dub
+ */
+public class VLCFXMediaPlayer extends Application{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {        
+        
+    }
+    
+    
+}
